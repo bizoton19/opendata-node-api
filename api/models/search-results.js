5496347b3684f29aa0b7614fd31999358d3c09fd
Alexandre Salomon <bizoton19@gmail.com>
2017-12-23 22:54:59 -0500

    added additional attributes to search model for generic search

---
+++ api/models/search-results.js
@@ -0,0 +1,10 @@
+module.exports = 
+function searchResult(){
+    this.artifacts=[],
+    this.aggregations=[],
+    this.docCount=0,
+    this.startPage=0,
+    this.totalPages=0,
+    this.took=0,
+    this.maxScore=0;
+}
